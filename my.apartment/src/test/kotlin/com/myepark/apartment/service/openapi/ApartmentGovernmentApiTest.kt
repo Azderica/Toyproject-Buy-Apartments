66696f9d2724d1130236c1034cc621988356d8c1
Azderica <mh97888@gmail.com>
2022-04-25 18:54:16 +0900

    일부 코드 검색

--- my.apartment/src/test/kotlin/com/myepark/apartment/service/openapi/ApartmentGovernmentApiTest.kt
+++ my.apartment/src/test/kotlin/com/myepark/apartment/service/openapi/ApartmentGovernmentApiTest.kt
@@ -9,13 +9,16 @@ import org.springframework.beans.factory.annotation.Autowired
 
 import org.springframework.boot.test.context.SpringBootTest
 
-@SpringBootTest
-class ApartmentGovernmentApiTest() {
-    @Autowired
-    private lateinit var apartmentGovernmentApi: ApartmentGovernmentApi
+// 참고 : https://github.com/daggerok/feign-client-examples/blob/0917758d1a39bc7ac5d6ecdb93ee16300613069f/user-json-feign-client-spring-cloud/src/test/kotlin/com/github/daggerok/openfeign/userjsonfeignclientspringcloud/UserClientTests.kt
+// https://github.com/fideism/gitee/blob/febda8ace2ca571534084b571004c674312446d1/archguard/archguard/src/test/kotlin/com/thoughtworks/archguard/code/module/domain/springcloud/feignclient/FeignClientServiceTest.kt
+
+@TestInstance(PER_CLASS)
+@AutoConfigureWireMock(port = 0)
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+class ApartmentGovernmentApiTest(@Autowired constructor(val apartmentGovernmentApi: ApartmentGovernmentApi)) {
 
     @Test
-    fun getApartmentRent() {
+    fun `정상적으로 아파트 전월세 내역 API를 호출하는지 확인`() {
         // given
         val req = ApartmentRentRequest(
             lawdCd = 41171,
@@ -31,7 +34,7 @@ class ApartmentGovernmentApiTest() {
     }
 
     @Test
-    fun getApartmentResaleRight() {
+    fun `정상적으로 아파트 분양권전매 내역 API를 호출하는지 확인`() {
         // given
         val req = ApartmentResaleRightRequest(
             lawdCd = 41171,
@@ -47,7 +50,7 @@ class ApartmentGovernmentApiTest() {
     }
 
     @Test
-    fun getApartmentSales() {
+    fun `정상적으로 아파트 매매 내역 API를 호출하는지 확인`() {
         // given
         val req = ApartmentSalesRequest(
             lawdCd = 41171,
@@ -63,7 +66,7 @@ class ApartmentGovernmentApiTest() {
     }
 
     @Test
-    fun getApartmentSalesDetail() {
+    fun `정상적으로 아파트 상세매매 내역 API를 호출하는지 확인`() {
         // given
         val req = ApartmentSalesDetailRequest(
             lawdCd = 41171,

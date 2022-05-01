1bf0f79ef701d9adcd822fc968ab9d83d0b4ebbc
Azderica <mh97888@gmail.com>
2022-05-01 17:25:40 +0900

    todo list 추가

---
+++ my.apartment/src/main/kotlin/com/myepark/apartment/controller/ApartmentController.java
@@ -0,0 +1,10 @@
+package com.myepark.apartment.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ApartmentController {
+    // todo. api 생성 필요
+    // 1. 지역 코드를 받으면 해당 호출 -> 종류별로
+
+}
